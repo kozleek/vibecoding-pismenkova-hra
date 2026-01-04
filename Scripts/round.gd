a72b8fe3d93df44d0cf3b9b65e905e36d5923df9
Tomáš Musiol <tomas.musiol@gmail.com>
2026-01-04 18:36:24 +0100

    Nastavení projektu

--- Scripts/round.gd
+++ Scripts/round.gd
@@ -8,7 +8,6 @@ signal round_finished
 @onready var progress_bar: ProgressBar = $MarginContainer/HBoxContainer/Panel/ProgressBar
 @onready var label: Label = $MarginContainer/HBoxContainer/Label
 
-var current_time: int = 0
 var remaining_time: int = 0
 
 func _ready() -> void:
@@ -20,6 +19,10 @@ func _ready() -> void:
 	label.text = str(remaining_time) + ' ' + tr("UI_SEC")
 
 	self.hide()
+
+# ========================
+# Ovládaní herních kol
+# ========================
 
 # Začátek kola
 func start() -> void:
@@ -38,18 +41,24 @@ func end() -> void:
 	timer_round_step.stop()
 	round_finished.emit()
 
+# ========================
+# Zobrazení stavu
+# ========================
+
 # Aktualizace progress baru podle zbývajícího času
 func _process(_delta: float) -> void:	
 	if not timer_round.is_stopped():
 		progress_bar.value = (1.0 - (timer_round.time_left / timer_round.wait_time)) * 100		
 	
+# ========================
+# Signály časovačů
+# ========================
+	
 # Spustí se když je kolo ukončeno
 func _on_timer_round_timeout() -> void:
-	end()
+	self.end()
 
 # Sekundový časovač pro odpočet kola - mění text v labelu u progress baru
 func _on_timer_round_step_timeout() -> void:
-	current_time += 1
 	remaining_time -= 1
-	
 	label.text = str(remaining_time) + ' ' + tr("UI_SEC")
